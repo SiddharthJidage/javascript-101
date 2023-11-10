5320cd3e0b04a23c0da94b9dc235b01d56ed7eb8
siddharth jidage <67370709+SiddharthJidage@users.noreply.github.com>
2023-11-10 10:15:30 +0000

    data types

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 3243423;
+let accountEmail = "sjidage3557@gmail.com";
+var accountPass = "xyz@2134";
+accountCity = "Pune";
+let accountState = null;
+
+// accountId = 22;
+accountEmail = "sjidage3555@gmail.com";
+accountPass = "xyz@1234";
+accountCity = "ICHALKARANJI";
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
+
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // trest all the JS code as newer version of JS.
+
+/*
+ datatpyes in JS
+
+
+ number
+ bigint(for big no) 
+ string, boolean
+ null(standalone value) its an object
+ undefined
+ sumbol(unique)
+
+
+*/
